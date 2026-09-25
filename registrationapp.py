c5cac89b6aedba94d1ee5b8f82dd584362db9afd
yeslordmj-hash <yeslordmj@gmail.com>
2026-09-25 13:53:02 +0000

    Fix environment variable access method for PORT

--- registrationapp.py
+++ registrationapp.py
@@ -1452,5 +1452,5 @@ def uploaded_file(filename):
 
 
 if __name__ == "__main__":
-  port = int(os.environ.K("PORT", 5001))
+  port = int(os.environ.get("PORT", 5001))
   app.run(host="0.0.0.0", port=port)
