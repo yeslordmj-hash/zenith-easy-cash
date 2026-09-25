25078c110ec76265a067acb9b27b939844b17fcc
yeslordmj-hash <yeslordmj@gmail.com>
2026-09-25 04:15:14 +0000

    Use environment variable for server port
    Updated app.run to use PORT environment variable.

--- registrationapp.py
+++ registrationapp.py
@@ -838,6 +838,7 @@ def admin_logout():
 def uploaded_file(filename):
   return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
-
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5001, debug=True)
+  port = int(os.environ.get("PORT", 5001))
+  app.run(host="0.0.0.0", port=port)
+
